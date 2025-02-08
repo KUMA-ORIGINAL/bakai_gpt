a56ce9f5b8a7adccd1d0c34e70cf32e94ae8afa1
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2025-02-08 08:28:51 +0600

    refact for prod

--- backend/app/alembic/env.py
+++ backend/app/alembic/env.py
@@ -30,7 +30,7 @@ target_metadata = Base.metadata
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
 
-config.get_main_option("DB_URL", settings.db.async_url)
+config.set_main_option('sqlalchemy.url', settings.db.async_url)
 
 
 def run_migrations_offline() -> None:
